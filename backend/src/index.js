237bec673788f85d6ecb09296111be9da222ba5e
Nishwan-Kumar <nishwan291205@gmail.com>
2026-01-21 22:40:43 +0530

    Fix catch-all route error

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"/",
     credentials:true
 }))
 
@@ -25,10 +25,12 @@ app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-  app.get("(.*)", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 

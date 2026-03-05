ff693c01b29dcb72483a657b7a6ce6dfb3a1ae0a
Nishwan-Kumar <165485530+Nishwan-Kumar@users.noreply.github.com>
2026-03-05 16:15:44 +0530

    Update Google OAuth callback URL

--- backend/src/lib/passport.js
+++ backend/src/lib/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `/api/auth/google/callback`,
+      callbackURL: `https://mern-chat-app-1-td9h.onrender.com/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
